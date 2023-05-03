502c279d1d3379fbd12310933807bce36bbbe280
treddy08 <treddy@redhat.com>
2023-05-03 18:58:37 +1000

    update

--- .workshop/scripts/default-settings.sh
+++ .workshop/scripts/default-settings.sh
@@ -1,6 +1,6 @@
 TERMINAL_IMAGE=quay.io/openshifthomeroom/workshop-terminal:3.4.2
 DASHBOARD_IMAGE=quay.io/openshifthomeroom/workshop-dashboard:5.0.0
-SPAWNER_IMAGE=quay.io/openshifthomeroom/workshop-spawner:7.1.0
+SPAWNER_IMAGE=quay.io/redhat-gpte/workshop-spawner:7.1.0
 CONSOLE_IMAGE=quay.io/openshift/origin-console:4.2
 
 NAME_PREFIX=
